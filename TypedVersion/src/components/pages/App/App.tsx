c3cd788c8aa5fdb533eb01d9d12e513e238c328d
Egor Kartashov <68367096+WildEgor@users.noreply.github.com>
2021-10-29 17:54:23 +0600

    feat: add template pages

--- TypedVersion/src/components/pages/App/App.tsx
+++ TypedVersion/src/components/pages/App/App.tsx
@@ -1,47 +1,14 @@
 import React from "react";
 import { observer } from 'mobx-react-lite'
-import InspectorService from "../../../core/services/inspector/inspector.service";
-import ReferenceImageBox from "../../organism/ReferenceImageBox";
-
-import { serverURI } from "../../../core/config/api.config";
-import LogImageList from "../../organism/LogImageList";
-import ImageBox from "../../molecules/ImageBox";
-import Toolbox from "../../organism/Toolbox";
-import MonitorData from "../../molecules/MonitorData";
 
 const App = observer(() => {
-  //const { notificationStore } = useContext(StoreContext);
-
-  const Inspector = InspectorService.getInstance(serverURI);
-
   return (
     <>
-    {/* <LogImageList
-      range={[0, 30]}
-      getImage={(id) => Inspector.getLogImage(id)}
-      lockLogger={(lock) => Inspector.setLogState(lock)}
-    /> */}
-    <ImageBox
-      getImage={Inspector.getLiveImage}
-    />
-    {/* <ReferenceImageBox
-      width={480}
-      height={320}
-      getMode={Inspector.getMode}
-      setMode={Inspector.setMode}
-      getRecipeImage={Inspector.getReferenceObject}
-      getActiveRecipeImage={Inspector.getActiveRecipe}
-      getRecipesNames={Inspector.getRecipeNames}
-      updateRecipe={Inspector.updateRecipe}
-      getActiveRecipeNumber={Inspector.getActiveReferenceIndex}
-      getRecipes={Inspector.getRecipeCount}
-    /> */}
-    {/* <Toolbox
-      defaultSettings={Inspector.defaultSettings}
-      getInt={Inspector.getInt}
-      setInt={Inspector.setInt}
-      getTools={Inspector.getTools}
-    />   */}
+      <div>
+            <a href="logger.html" id="supervision" title="Check the status of the camera">Logger</a>
+            <a href="liveviewer.html" id="maintenance" title="Device control for field exchange">Live</a>
+            <a href="changereference.html" id="batchchange" title="Switch reference object and fine-tune parameters">Change Reference</a>
+        </div>
     </>
   )
 })
